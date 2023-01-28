4b9645ad92fd3b5f15efc4885d01078b9a086baa
lpurdy01 <ljpurdy@alaska.edu>
2023-01-28 08:54:39 -0900

    Auth blueprint added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -44,8 +44,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # register the database commands
     from . import db
     db.init_app(app)
+
+    # register the auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     # a simple page that says hello
     @app.route('/hello')
